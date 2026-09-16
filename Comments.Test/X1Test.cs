26b50f3d16afed9a7439b7efbc49d24c2046ac82
Charlie Howlett <charlie.howlett@mindlinksoft.com>
2026-09-16 10:42:51 +0100

    It works!

--- Comments.Test/X1Test.cs
+++ Comments.Test/X1Test.cs
@@ -6,20 +6,30 @@ using NUnit.Framework;
 public class X1Test
 {
     [Test]
-    public void T1()
+    public void CalculatingSumOfSquaresGivenRangeCalculatesSumOfSquares()
     {
+        int calculateExpectedSumOfSquares(int startRange, int endRange)
+        {
+            int expectedSumOfSquares = 0;
+            for (int i = startRange; i <= endRange; i++)
+            {
+                expectedSumOfSquares += i * i;
+            }
+
+            return expectedSumOfSquares;
+        }
+
         int startRange = 7;
         int endRange = 12;
 
-        // Expected: sum of squares from 7 to 12
-        int expectedSumOfSquares = 0;
-        for (int i = startRange; i <= endRange; i++)
-        {
-            expectedSumOfSquares += i * i;
-        }
+        var expectedSumOfSquares = calculateExpectedSumOfSquares(
+            startRange,
+            endRange);
 
         int actualSumOfSquares = X1.M(startRange, endRange);
 
-        Assert.That(actualSumOfSquares, Is.EqualTo(expectedSumOfSquares));
+        Assert.That(
+            actualSumOfSquares,
+            Is.EqualTo(expectedSumOfSquares));
     }
 }
